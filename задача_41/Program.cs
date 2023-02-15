205ed2b873b60ffed7ed8eb6586418dae1194c1d
Artem Antonenko <a-.-@MacBook-Pro-Artem.local>
2023-02-15 17:33:27 +0300

    first commit

---
+++ задача_41/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+string elements = Console.ReadLine();
+int[] array = GetArrayFromString(elements);
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int find (int[] array)
+{
+    int count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+Console.WriteLine($"{find (array)}");
